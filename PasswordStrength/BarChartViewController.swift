cf0959bfb5f615a27e6c472b54a12b7a8c7ad32a
C00IHandLuke <dylanirur@outlook.com>
2015-11-29 18:08:42 -0800

    close to finished

---
+++ PasswordStrength/BarChartViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BarChartViewController.swift
+//  PasswordStrength
+//
+//  Created by Dylan Iruretagoyena on 2015-11-29.
+//  Copyright © 2015 Micheal Griffin. All rights reserved.
+//
+
+import Foundation
---
+++ PasswordStrength/WebViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WebViewController.swift
+//  PasswordStrength
+//
+//  Created by Dylan Iruretagoyena on 2015-11-29.
+//  Copyright © 2015 Micheal Griffin. All rights reserved.
+//
+
+import Foundation
