a894643910e09ca0db758bb17b3cbc6e6a1dd954
C00IHandLuke <dylanirur@outlook.com>
2015-11-29 18:42:21 -0800

    closer to finished

--- PasswordStrength/BarChartViewController.swift
+++ PasswordStrength/BarChartViewController.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+//
+//  BarChartViewController.swift
+//  iOSChartsDemo
+//
+//  Created by Joyce Echessa on 6/12/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class BarChartViewController: UIViewController {
+    
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var months: [String]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        months = ["Strong", "Weak", "Recomended", "Yours"]
+        let unitsSold = [500.0, 100.0, 300.0, 700.0]
+        
+        setChart(months, values: unitsSold)
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart"
+        barChartView.noDataTextDescription = "You need to enter a valid password"
+        
+        
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Time in days")
+        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+        barChartView.data = chartData
+        
+        barChartView.descriptionText = ""
+        
+        barChartView.xAxis.labelPosition = .Bottom
+        
+        chartDataSet.colors = [UIColor(red: 100/255, green: 200/255, blue: 10/255, alpha: 1), UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1),UIColor(red: 0/255, green: 0/255, blue: 255/255, alpha: 1), UIColor(red: 100/255, green: 100/255, blue: 100/255, alpha: 1)]
+        
+        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+        
+        let ll = ChartLimitLine(limit: 500.0, label: "")
+        barChartView.rightAxis.addLimitLine(ll)
+        let ii = ChartLimitLine(limit: 100.0, label: "")
+        barChartView.rightAxis.addLimitLine(ii)
+        
+        
+    }
+    
+    
+}
+
--- PasswordStrength/WebViewController.swift
+++ PasswordStrength/WebViewController.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import UIKit
+import WebKit
+
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+    
+    
+   // @IBOutlet weak var openInSafari: UIWebView!
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "https://support.google.com/accounts/answer/32040?hl=en")!
+        webView.loadRequest(NSURLRequest(URL: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+}
